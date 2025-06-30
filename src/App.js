1722d4cf260caa4fabe9e832c1644bee07eaf9e4
Prasit7 <159733083+Prasit7@users.noreply.github.com>
2025-06-30 16:02:01 -0500

    testing deployment Real

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There, this is a React app running in a Docker container!
+          Greetings from the React app running in a Docker container!
         </p>
         <a
           className="App-link"
